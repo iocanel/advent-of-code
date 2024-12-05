4a80620d51d6d3b43624aac67cc46fb8d2fb2f3d
Ioannis Canellos <iocanel@gmail.com>
2024-12-05 10:59:49 +0200

    chore: code improvements

--- 2024/day05/a.py
+++ 2024/day05/a.py
@@ -1,39 +1,35 @@
 import sys
 
-with open(sys.argv[1]) as f:
-    lines = f.readlines()
-
-page_ordering_rules = [[int(v) for v in line.strip().split('|')] for line in lines if '|' in line]
-updates = [[int(v) for v in line.strip().split(',')] for line in lines if ',' in line]
-
-#print(page_ordering_rules)                    
-#print(updates)
-
 def find_middle_page(update):
     return update[len(update) // 2]
 
-
 def is_update_valid(update, page_ordering_rules):
     for rule in page_ordering_rules:
-        first = rule[0]
-        second = rule[1]
-        first_index = -1 
-        second_index = -1
-        if first in update:
+        first, second = rule
+        try:
             first_index = update.index(first)
-        if second in update:
             second_index = update.index(second)
-        if first_index > second_index and second_index != -1 and first_index != -1:
-            #print(f'Invalid update: {update} violates rule {rule}')
+        except ValueError:
+            continue
+        if first_index > second_index:
             return False
     return True
 
+def main(file_path):
+    with open(file_path) as f:
+        lines = f.readlines()
 
-sum = 0
-for update in updates:
-    if is_update_valid(update, page_ordering_rules):
-        page =  find_middle_page(update)
-        #print(f'Middle Page: {page}')
-        sum += page
-print(sum)
+    page_ordering_rules = [[int(v) for v in line.strip().split('|')] for line in lines if '|' in line]
+    updates = [[int(v) for v in line.strip().split(',')] for line in lines if ',' in line]
+
+    total_sum = 0
+    for update in updates:
+        if is_update_valid(update, page_ordering_rules):
+            page =  find_middle_page(update)
+            total_sum += page
+    print(total_sum)
+
+if __name__ == '__main__':
+    main(sys.argv[1])
+
 
--- 2024/day05/b.py
+++ 2024/day05/b.py
@@ -1,13 +1,5 @@
+from os import truncate
 import sys
-
-with open(sys.argv[1]) as f:
-    lines = f.readlines()
-
-page_ordering_rules = [[int(v) for v in line.strip().split('|')] for line in lines if '|' in line]
-updates = [[int(v) for v in line.strip().split(',')] for line in lines if ',' in line]
-
-#print(page_ordering_rules)                    
-#print(updates)
 
 def find_middle_page(update):
     return update[len(update) // 2]
@@ -15,21 +7,18 @@ def find_middle_page(update):
 
 def is_update_valid(update, page_ordering_rules):
     for rule in page_ordering_rules:
-        first = rule[0]
-        second = rule[1]
-        first_index = -1 
-        second_index = -1
-        if first in update:
+        first, second = rule
+        try:
             first_index = update.index(first)
-        if second in update:
             second_index = update.index(second)
-        if first_index > second_index and second_index != -1 and first_index != -1:
-            #print(f'Invalid update: {update} violates rule {rule}')
+        except ValueError:
+            continue
+        if first_index > second_index:
             return False
     return True
 
 
-def compare(a, b):
+def compare(a, b, page_ordering_rules):
     for rule in page_ordering_rules:
         if rule[0] == a and rule[1] == b:
             return False
@@ -37,21 +26,27 @@ def compare(a, b):
             return True 
     return True
 
-def sort(array):
+def sort(array, page_ordering_rules):
     result = [i for i in array]
     for i in range(len(result)):
         for j in range(i, len(result)):
-            if compare(result[i], result[j]):
+            if compare(result[i], result[j], page_ordering_rules):
                 result[i], result[j] = result[j], result[i]
     return result
 
-sum = 0
-for update in updates:
-    if not is_update_valid(update, page_ordering_rules):
-        sorted_update = sort(update)
-        #print(f'update: {update} sorted: {sorted_update}')
-        page =  find_middle_page(sorted_update)
-        #print(f'Middle Page: {page}')
-        sum += page
-print(sum)
+def main(file_path):
+    with open(sys.argv[1]) as f:
+        lines = f.readlines()
+    
+    page_ordering_rules = [[int(v) for v in line.strip().split('|')] for line in lines if '|' in line]
+    updates = [[int(v) for v in line.strip().split(',')] for line in lines if ',' in line]
+    sum = 0
+    for update in updates:
+        if not is_update_valid(update, page_ordering_rules):
+            sorted_update = sort(update, page_ordering_rules)
+            page =  find_middle_page(sorted_update)
+            sum += page
+    print(sum)
 
+if __name__ == '__main__':    
+    main(sys.argv[1])
